0a79c731ffbbb315fdc66bf23a41b1e05a3ad572
최승리 <choeseungli@choeseunglis-MacBook-Pro.local>
2022-02-25 00:02:02 +0900

    modify:settings 분리

--- config/settings/base.py
+++ config/settings/base.py
@@ -12,10 +12,10 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 
-from local_settings import SECRET_KEY, DATABASES
+from settings import SECRET_KEY, DATABASES
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
